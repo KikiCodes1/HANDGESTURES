18be8ba15c8b22d7ae1fd0a7317131ec55364c0b
KikiCodes1 <95891807+KikiCodes1@users.noreply.github.com>
2022-12-05 18:49:24 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -31,3 +31,34 @@ function speak(){
     var utterThis = new SpeechSynthesisUtterance(speak_data);
     synth.speak(utterThis);
 }
+
+function check(){
+    img = document.getElementById("result2");
+    classifier.classify(img, gotResult);
+}
+
+function gotResult(error, results){
+    if (error){
+        console.error(error);
+} else{
+    console.log(results);
+    document.getElementById("gesture_name").innerHTML = results[0].label;
+    prediction = results[0].label;
+    speak();
+    if(results[0].label == "Peace Sign"){
+        document.getElementById("gesture_image").innerHTML = "&#9996;";
+    }
+    if(results[0].label == "Thumbs Up"){
+        document.getElementById("gesture_image").innerHTML = "&#128077;";
+    }
+    if(results[0].label == "Thumbs Down"){
+        document.getElementById("gesture_image").innerHTML = "&#128078;";
+    }
+    if(results[0].label == "Very Good Sign"){
+        document.getElementById("gesture_image").innerHTML = "&#128076;";
+    }
+    if(results[0].label == "Stop Sign"){
+        document.getElementById("gesture_image").innerHTML = "&#9995;";
+    }
+}
+}
